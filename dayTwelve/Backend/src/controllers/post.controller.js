bb30de92891e0407c21cfb3deeddf372a20a6ab1
hamimbsf <hamim.bsf@gmail.com>
2026-03-25 13:08:09 +0600

    file uploading bug fixxed

--- dayTwelve/Backend/src/controllers/post.controller.js
+++ dayTwelve/Backend/src/controllers/post.controller.js
@@ -2,20 +2,39 @@ const postModel = require("../models/post.model");
 
 const ImageKit = require("@imagekit/nodejs");
 const { toFile } = require("@imagekit/nodejs");
+const jwt = require("jsonwebtoken");
 
 const client = new ImageKit({
   privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
 });
 
 const createPostController = async (req, res) => {
-  console.log(req.body, req.file);
+  const { caption } = req.body;
+  const token = req.cookies.token;
+  if (!token) {
+    res.status(401).json({
+      message: "token not provided,unauthorized access",
+    });
+  }
+
+  const decode = jwt.verify(token, process.env.JWT_SECRET);
+  // return console.log(decode);
 
   const file = await client.files.upload({
     file: await toFile(Buffer.from(req.file.buffer), "file"),
     fileName: "Test",
   });
 
-  res.send(file);
+  const post = await postModel.create({
+    caption: caption,
+    imgUrl: file.url,
+    user: decode.id,
+  });
+
+  res.status(201).json({
+    message: "Post created successfully",
+    post,
+  });
 };
 
 module.exports = {
