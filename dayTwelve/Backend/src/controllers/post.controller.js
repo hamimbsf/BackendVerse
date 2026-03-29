19203a5c89b3d8d6c4d6341d3aeb743cc9a5eae0
hamimbsf <hamim.bsf@gmail.com>
2026-03-29 18:28:34 +0600

    added like features

--- dayTwelve/Backend/src/controllers/post.controller.js
+++ dayTwelve/Backend/src/controllers/post.controller.js
@@ -2,6 +2,7 @@ const postModel = require("../models/post.model");
 
 const ImageKit = require("@imagekit/nodejs");
 const { toFile } = require("@imagekit/nodejs");
+const likeModel = require("../models/like.model");
 
 const client = new ImageKit({
   privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
@@ -63,8 +64,47 @@ async function getPostDetailsController(req, res) {
   });
 }
 
+const likePostController = async (req, res) => {
+  const username = req.user.username;
+  const postId = req.params.postId;
+
+  const post = await postModel.findById(postId);
+
+  if (!post) {
+    return res.status(404).json({
+      message: "Post not found",
+    });
+  }
+
+  // check if already liked
+  const existingLike = await likeModel.findOne({
+    post: postId,
+    user: username,
+  });
+
+  // 👉 UNLIKE
+  if (existingLike) {
+    await likeModel.findByIdAndDelete(existingLike._id);
+
+    return res.status(200).json({
+      message: "Post unliked",
+    });
+  }
+
+  const like = await likeModel.create({
+    post: postId,
+    user: username,
+  });
+
+  res.status(200).json({
+    message: " Post like successfully",
+    like,
+  });
+};
+
 module.exports = {
   createPostController,
   getPostController,
   getPostDetailsController,
+  likePostController,
 };
--- dayTwelve/Backend/src/routes/post.routes.js
+++ dayTwelve/Backend/src/routes/post.routes.js
@@ -4,19 +4,31 @@ const {
   createPostController,
   getPostController,
   getPostDetailsController,
+  likePostController,
 } = require("../controllers/post.controller");
 const identifyUser = require("../middleware/auth.middleware");
 
 const multer = require("multer");
 const upload = multer({ storage: multer.memoryStorage() });
 
+/*  */
 postRouter.post(
   "/",
   upload.single("image"),
   identifyUser,
   createPostController,
 );
+
+/*  */
 postRouter.get("/", identifyUser, getPostController);
+
+/*  */
 postRouter.get("/details/:postId", identifyUser, getPostDetailsController);
+
+/**
+ * like a post
+ * */
+
+postRouter.post("/like/:postId", identifyUser, likePostController);
 
 module.exports = postRouter;
